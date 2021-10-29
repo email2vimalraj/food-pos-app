637401e638ce40fb44cc70c14fc20d236ed61fef
Vimalraj Selvam <email2vimalraj@gmail.com>
2021-10-29 10:21:30 +0530

    Added icons

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -29,13 +29,50 @@ const SideBar = () => {
         </a>
 
         {/* nav */}
-        <nav>
-          <a href="" className="block py-2.5 px-4 hover:bg-gray-700 rounded hover:text-gray-50 transition duration-200">Dashboard</a>
-          <a href="" className="block py-2.5 px-4 hover:bg-gray-700 rounded hover:text-gray-50 transition duration-200">Sell</a>
-          <a href="" className="block py-2.5 px-4 hover:bg-gray-700 rounded hover:text-gray-50 transition duration-200">Receipts</a>
-          <a href="" className="block py-2.5 px-4 hover:bg-gray-700 rounded hover:text-gray-50 transition duration-200">Petty Cash</a>
-          <a href="" className="block py-2.5 px-4 hover:bg-gray-700 rounded hover:text-gray-50 transition duration-200">Products</a>
-          <a href="" className="block py-2.5 px-4 hover:bg-gray-700 rounded hover:text-gray-50 transition duration-200">Settings</a>
+        <nav className="text-gray-400">
+          <a href="/" className="block py-4 px-4 hover:bg-gray-700 rounded hover:text-gray-50 transition duration-200">
+            <div className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+              </svg>&nbsp;Dashboard
+            </div>
+          </a>
+          <a href="/" className="block py-4 px-4 hover:bg-gray-700 rounded hover:text-gray-50 transition duration-200">
+            <div className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+              </svg>&nbsp;Sell
+            </div>
+          </a>
+          <a href="/" className="block py-4 px-4 hover:bg-gray-700 rounded hover:text-gray-50 transition duration-200">
+            <div className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clipRule="evenodd" />
+              </svg>&nbsp;Receipts
+            </div>
+          </a>
+          <a href="/" className="block py-4 px-4 hover:bg-gray-700 rounded hover:text-gray-50 transition duration-200">
+            <div className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clipRule="evenodd" />
+                <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
+              </svg>&nbsp;Petty Cash
+            </div>
+          </a>
+          <a href="/" className="block py-4 px-4 hover:bg-gray-700 rounded hover:text-gray-50 transition duration-200">
+            <div className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
+              </svg>&nbsp;Products
+            </div>
+          </a>
+          <a href="/" className="block py-4 px-4 hover:bg-gray-700 rounded hover:text-gray-50 transition duration-200">
+            <div className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
+              </svg>&nbsp;Settings
+            </div>
+          </a>
         </nav>
       </div>
     </>
